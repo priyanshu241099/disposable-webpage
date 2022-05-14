2a9a8cca10f4bf24df44ade06b3c26704bf83599
priyanshu241099 <priyanshuv62@gmail.com>
2022-05-14 08:10:58 +0530

    ..

--- frontend/src/components/user/manageAsset.js
+++ frontend/src/components/user/manageAsset.js
@@ -1,9 +1,12 @@
+import { CardContent } from '@mui/material'
 import React from 'react'
 
 const ManageAsset = () => {
   return (
     <div>ManageAsset</div>
   )
+  
+  
 }
 
 export default ManageAsset
